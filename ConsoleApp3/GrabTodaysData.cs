bd80fd5b0069b624bc8839e50333a084218243ef
Zachary Schalberg <zschalberg@gmail.com>
2019-04-27 22:29:10 -0400

    Initial Comit

---
+++ ConsoleApp3/GrabTodaysData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    class GrabTodaysData
+    {
+        public static string GrabData(int CurrentYear)
+        {
+            string URL = $"https://statsapi.web.nhl.com/api/v1/game/{CurrentYear}030111/boxscore";
+
+
+            WebClient wc = new WebClient();
+            string webData = wc.DownloadString(URL);
+            return webData;
+        }
+        
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int CurrentYear = DateTime.Now.Year - 1;
+            string Game = GrabTodaysData.GrabData(CurrentYear);
+        }
+    }
+}
